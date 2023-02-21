4519ab0a67fa6f59b70b1662e88137865a493b35
nabganari <nabilahganari@gmail.com>
2023-02-21 19:16:41 +0700

    styling css todos should final

--- js/main.js
+++ js/main.js
@@ -24,10 +24,19 @@ function displayTodo() {
 
     let htmlCode = "";
     todoArray.forEach((list, ind) => {
-        htmlCode += `<div>
-        <p>${list}</p>
-        <button class="btn btn-outline-secondary" onclick='edit(${ind})' type="button">Edit</button>
-        <button class="btn btn-outline-secondary" onclick='deleteTodo(${ind})' type="button">Delete</button>
+        htmlCode += `<div class="d-relative row ">
+        <div class="col-4"></div>
+        <div class=" col-6 mt-4 justify-content-center border border-secondary rounded"
+            style="width: 500px; height: 40px;">
+            <p class=" p-2" style=" color: black;">${list}</p>
+        </div>
+        <div class="col-2 px-5 mt-4 d-flex justify-content-center">
+            <button class=" btn btn-outline-secondary ms-2 px-5" style=" background-color: #069662; color: white;"
+                onclick='edit(${ind})' type="button">Edit</button>
+            <button class="btn btn-outline-secondary ms-2 px-4" style="background-color: #CC4242;color: white;"
+                onclick='deleteTodo(${ind})' type=" button">Delete</button>
+        </div>
+        <div class="col-1"></div>
     </div>`;
     });
     listBox.innerHTML = htmlCode;
